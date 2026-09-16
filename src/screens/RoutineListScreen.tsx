6881e2c9c03a6b7667d8f1056bdfc3b4ca13588a
Junior27VQ <favq1999@outlook.com>
2026-09-15 23:23:21 -0500

    fead: Connecting Deep Navigation

--- src/screens/RoutineListScreen.tsx
+++ src/screens/RoutineListScreen.tsx
@@ -1,11 +1,15 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function RoutineListScreen(){
+export default function RoutineListScreen({navigation}: any){
     return (
       <SafeAreaView>
         <View>
           <Text>Lista de Rutinas</Text>
+          <Button
+            title='Ver Rutina de Pecho'
+            onPress={()=> navigation.navigate('Detail')}
+          />
         </View>
       </SafeAreaView>
     )
